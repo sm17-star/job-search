b25109c2db92521df23a14022f9333f4d154136c
user <253704673+sm17-star@users.noreply.github.com>
2026-09-06 01:12:10 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const allowedOrigins = [
-  "http://localhost:5173",
+  // "http://localhost:5173",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
